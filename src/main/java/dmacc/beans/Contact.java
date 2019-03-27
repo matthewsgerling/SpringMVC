116ea122f57cf55c51e96c45565c2ae80234d755
Bones707 <matthewsgerling@gmail.com>
2019-03-27 09:53:54 -0500

    All Files Created

--- src/main/java/dmacc/beans/Contact.java
+++ src/main/java/dmacc/beans/Contact.java
@@ -21,31 +21,38 @@ public class Contact {
 		super();
 		this.relationship = "spouse";
 	}
+	
 	public Contact(String name) {
 		super();
 		this.name = name;
 	}
+	
 	public Contact(String name, String phone, String relationship) {
 		super();
 		this.name = name;
 		this.phone = phone;
 		this.relationship = relationship;
 	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getPhone() {
 		return phone;
 	}
+	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
 	public String getRelationship() {
 		return relationship;
 	}
+	
 	public void setRelationship(String relationship) {
 		this.relationship = relationship;
 	}
@@ -53,13 +60,22 @@ public class Contact {
 	public Address getAddress() {
 		return address;
 	}
+	
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
 		return "Contact [name=" + name + ", phone=" + phone + ", relationship=" + relationship
 				+ ", address=" + address + "]";
 	}
-
 }
